da48675cba8b19401dc0b828f555ede45211f444
Marília Leite Franca <ma1088@hotmail.com>
2023-12-23 00:00:53 -0300

    corrigindo erros de validação do DTO

--- src/main/java/com/api/parkingcontrol/dtos/responsibleperson/ResponsiblePersonDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/responsibleperson/ResponsiblePersonDto.java
@@ -3,6 +3,7 @@ package com.api.parkingcontrol.dtos.responsibleperson;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,17 +16,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ResponsiblePersonDto {
 
+    private static final String OR = "|";
+    private static final String MASK_LANDLINE_PHONE = "^\\(\\d{2}\\)\\d{4}-\\d{4}$";
+    private static final String MASK_MOBILE_PHONE = "^\\(\\d{2}\\)\\d{5}-\\d{4}$";
+    private static final String MASK_CPF = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$";
+    
+
     @NotBlank
-    @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$")
+    @Pattern(regexp = MASK_CPF)
     private String cpf;
 
     @NotBlank
     private String name;
 
-    @NotBlank
+    @NotNull
     private LocalDate birthDate;
 
     @NotBlank
-    @Pattern(regexp = "^(\\(\\d{2}\\)\\d{4}-\\d{4}|\\(d{2}\\)d{5}\\-d{4})$")
+    @Pattern(regexp = MASK_LANDLINE_PHONE + OR + MASK_MOBILE_PHONE)
     private String phoneNumber;
 }
